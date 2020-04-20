1e3890cdf4427991c1a09e1814be613c59735e8f
Zach Ayers <z.ayers@icloud.com>
2020-04-20 17:59:40 -0500

    init commit

---
+++ utils.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// Rate limiting function
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
